c7a2415b91c964d54e49c8607f8ea848c79f6000
ajhipsher <github.lcgx7@simplelogin.com>
2025-11-19 10:52:45 -0800

    Made a triangle.

--- main.py
+++ main.py
@@ -2,23 +2,31 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"""Screen width: {SCREEN_WIDTH}
 Screen height: {SCREEN_HEIGHT}""")
-    run = 0
-    while run == 0:
+
+    player = Player(SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    running = True
+    while running:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
